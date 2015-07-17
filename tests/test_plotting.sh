7fae48cd235a1397766c82c4402ca5a42c9c95e0
Lauri Kovanen <lauri.kovanen@gmail.com>
2015-07-17 10:30:26 +0000

    Made it easier to change python version

--- tests/test_plotting.sh
+++ tests/test_plotting.sh
@@ -17,12 +17,14 @@ min_count=10 # Motif must have a count of at least min_count to be included.
 
 test_output="test_small_output.dat"
 
+python="python"
+
 # Plot all motifs ordered by motif count.
-python ${plot_code} --most_common -df ${test_output} -tf ${type_desc} --prefix ${test_output/.dat} -nt ${nodes} -et ${events} --verbose --label ${label_def} --order_by ${order_by} -or
+${python} ${plot_code} --most_common -df ${test_output} -tf ${type_desc} --prefix ${test_output/.dat} -nt ${nodes} -et ${events} --verbose --label ${label_def} --order_by ${order_by} -or
 
 # Plot all 2-event motifs, each topology in a separate figure
-python ${plot_code} --most_common_grouped_single -e 2 -df ${test_output} -tf ${type_desc} --prefix ${test_output/.dat} -nt ${nodes} -et ${events} --verbose --label ${label_def} --group_by ${group_by} --order_by ${order_by} -or
+${python} ${plot_code} --most_common_grouped_single -e 2 -df ${test_output} -tf ${type_desc} --prefix ${test_output/.dat} -nt ${nodes} -et ${events} --verbose --label ${label_def} --group_by ${group_by} --order_by ${order_by} -or
 
 # Plot all motifs in a single figure, grouped by topology and sorted
 # by count in each group.
-python ${plot_code} --most_common_grouped -df ${test_output} -tf ${type_desc} --prefix ${test_output/.dat} -nt ${nodes} -et ${events} --verbose --label ${label_def} --group_by ${group_by} --order_by ${order_by} -or
+${python} ${plot_code} --most_common_grouped -df ${test_output} -tf ${type_desc} --prefix ${test_output/.dat} -nt ${nodes} -et ${events} --verbose --label ${label_def} --group_by ${group_by} --order_by ${order_by} -or
